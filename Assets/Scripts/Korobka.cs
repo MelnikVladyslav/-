0f91c8e63c168e8c3cc47442e0a0a49dec9c73b3
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2024-04-21 15:04:10 +0300

    final update

---
+++ Assets/Scripts/Korobka.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Korobka : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag("Player"))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,9 +7,72 @@ namespace Assets.Scripts.Managers
 {
     public class LevelManager : MonoBehaviour
     {
+        int levelSucces = -1;
+        float time = 55;
+
+        #region levelsUI
+
+        public GameObject plRost;
+        public GameObject level00;
+        public GameObject level011;
+        public GameObject level012;
+        public GameObject level02;
+        public GameObject panelQuest;
+
+        #endregion
+
+        private void Start()
+        {
+            levelSucces = PlayerPrefs.GetInt("levelSuc");
+            if (PlayerPrefs.GetFloat("time") != 0)
+            {
+                time = PlayerPrefs.GetFloat("time");
+            }
+        }
+
+        private void Update()
+        {
+            if (levelSucces == 0)
+            {
+                level00.SetActive(false);
+                panelQuest.SetActive(true);
+                plRost.SetActive(true);
+                levelSucces = -1;
+                PlayerPrefs.SetInt("levelSuc", -1);
+            }
+            if (levelSucces == 1)
+            {
+                level00.SetActive(false);
+                level011.SetActive(false);
+                level012.SetActive(false);
+                level02.SetActive(true);
+                plRost.SetActive(true);
+                levelSucces = -1;
+                PlayerPrefs.SetInt("levelSuc", -1);
+            }
+        }
+
+        public void EnterRoad(int i)
+        {
+            if (i == 1)
+            {
+                panelQuest.SetActive(false);
+                level011.SetActive(true);
+                plRost.transform.position = level011.transform.position;
+            }
+            if (i == 2)
+            {
+                panelQuest.SetActive(false);
+                level012.SetActive(true);
+                plRost.transform.position = level012.transform.position;
+            }
+        }
+
         public void EnterLevel(int id)
         {
             SceneManager.LoadSceneAsync(id);
+
+            PlayerPrefs.SetFloat("time", time);
         }
     }
 }
